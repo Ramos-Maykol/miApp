184957c19d227076138ccb7a59519a183c3da724
Ramos-Maykol <mramosp@unitru.edu.pe>
2025-10-06 19:03:59 -0500

    Versión 01

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,9 +2,9 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonButton, IonText, IonIcon } from '@ionic/angular/standalone';
+import { MenuController, IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { logInOutline } from 'ionicons/icons';
+import { logInOutline, storefrontOutline } from 'ionicons/icons';
 
 // Importa tu servicio de autenticación
 import { AuthService } from '../../services/auth';
@@ -15,15 +15,16 @@ import { AuthService } from '../../services/auth';
   styleUrls: ['./login.page.scss'],
   standalone: true,
   imports: [
-    IonIcon, IonButton, IonInput, IonItem, IonList,
-    IonContent, IonHeader, IonTitle, IonToolbar,
-    CommonModule, ReactiveFormsModule, RouterLink
-  ]
+  IonIcon, IonButton, IonInput, IonItem,
+  IonContent,
+  CommonModule, ReactiveFormsModule, RouterLink
+]
 })
 export class LoginPage {
-  // Inyectamos el servicio de autenticación y el router
+  // Inyectamos los servicios necesarios
   private authService = inject(AuthService);
   private router = inject(Router);
+  private menuCtrl = inject(MenuController);
 
   // Creamos el formulario con validaciones
   loginForm = new FormGroup({
@@ -32,20 +33,28 @@ export class LoginPage {
   });
 
   constructor() {
-    // Agregamos el ícono que usaremos en la vista
-    addIcons({ logInOutline });
+    // Agregamos los íconos que usaremos en la vista
+    addIcons({ logInOutline, storefrontOutline });
   }
 
-  // --- Función que se llama al presionar el botón de ingresar ---
+  // Se ejecuta ANTES de que la página entre en la vista
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false);
+  }
+
+  // Se ejecuta ANTES de que la página salga de la vista
+  ionViewWillLeave() {
+    this.menuCtrl.enable(true);
+  }
+
+  // Función que se llama al presionar el botón de ingresar
   async login() {
-    // Verificamos si el formulario es válido
     if (this.loginForm.valid) {
       try {
-        // Llamamos a la función login() del servicio de autenticación
         await this.authService.login(this.loginForm.value);
       } catch (error) {
-        // Aquí podrías mostrar una alerta al usuario si el login falla
         console.error('Error en el login', error);
+        // Aquí podrías agregar una alerta para el usuario
       }
     } else {
       console.log('Formulario no válido');
